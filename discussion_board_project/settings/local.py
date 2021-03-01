bc9ef6099910809d4451085190c4dff47baf8eb5
willcod <william.wow@icloud.com>
2021-03-01 22:40:19 +0800

    move settings to project folder

---
+++ discussion_board_project/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '+r8l_vu^k(i*upia)r$rclaul2$59we6l7f=v#p-f90rod92@z'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
